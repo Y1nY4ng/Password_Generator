2884c8dc0dccbf678995e2ffd5ff51bd47aac510
Y1n_yng Hong <NathanYang557@gmail.com>
2025-02-11 20:01:05 +0000

    added some more stuff

--- password.py
+++ password.py
@@ -23,4 +23,15 @@ def fetch_word():
     word = response.json()[0]
     return word
 
-print(fetch_word())
+def replaceLetter():
+    word = word[0].upper() + word[1:]
+    if "a" in word:
+        word = word.replace("a", "@")
+
+def generate_weaker_password():
+    word1 = fetch_word()
+    word2 = fetch_word()
+    password = word1 + word2
+    return password
+    
+print(generate_weaker_password())
