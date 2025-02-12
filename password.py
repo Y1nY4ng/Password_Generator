ad1b5d2815722e35e4be8b59f49ff94143875cfd
Y1n_yng Hong <NathanYang557@gmail.com>
2025-02-12 19:35:19 +0000

    finished and fixed the code

--- password.py
+++ password.py
@@ -1,5 +1,6 @@
 import random
 import string
+import requests
 
 #function that generates a single random character
 def random_character():
@@ -23,14 +24,27 @@ def fetch_word():
     word = response.json()[0]
     return word
 
-def replaceLetter():
+def replaceLetters(word):
     word = word[0].upper() + word[1:]
     if "a" in word:
         word = word.replace("a", "@")
+    if "n" in word:
+        word = word.replace("n", "^")
+    if "i" in word:
+        word = word.replace("i", "1")
+    if "j" in word:
+        word = word.replace("j", "!")
+    if "s" in word:
+        word = word.replace("s", "$")
+
+
+    return word
 
 def generate_weaker_password():
     word1 = fetch_word()
     word2 = fetch_word()
+    word1 = replaceLetters(word1)
+    word2 = replaceLetters(word2)
     password = word1 + word2
     return password
     
